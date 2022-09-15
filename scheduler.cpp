5f48e471f4637afc7ac5ef43189a712813d55937
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2022-09-15 17:10:21 +0930

    RandomTomato: Add comments and refactor scheduler

--- scheduler.cpp
+++ scheduler.cpp
@@ -1,26 +1,28 @@
+// Program Imports
 #include <iostream>
 #include <fstream>
 #include <deque>
 #include <vector>
 #include <algorithm>
 
+// Program Constants
 // std is a namespace: https://www.cplusplus.com/doc/oldtutorial/namespaces/
 const int TIME_ALLOWANCE = 8;  // allow to use up to this number of time slots at once
 const int PRINT_LOG = 0; // print detailed execution trace
 const int HIGH_PRIORITY = 0;
 const int LOW_PRIORITY = 1;
 
-class Customer
-{
-public:
-    std::string name;
-    int priority;
-    int arrival_time;
-    int slots_remaining; // how many time slots are still needed
-    int playing_since;
-
-    Customer(std::string par_name, int par_priority, int par_arrival_time, int par_slots_remaining)
-    {
+// Customer Class
+class Customer {
+    // Object Variables
+    public:
+        std::string name;
+        int priority;
+        int arrival_time;
+        int slots_remaining; // how many time slots are still needed
+        int playing_since;
+    // Customer Constructor
+    Customer(std::string par_name, int par_priority, int par_arrival_time, int par_slots_remaining) {
         name = par_name;
         priority = par_priority;
         arrival_time = par_arrival_time;
@@ -29,50 +31,39 @@ public:
     }
 };
 
-class Event
-{
-public:
-    int event_time;
-    int customer_id;  // each event involes exactly one customer
-
-    Event(int par_event_time, int par_customer_id)
-    {
+// Event Class
+class Event {
+    // Object Variables
+    public:
+        int event_time;
+        int customer_id;  // each event involes exactly one customer
+    // Event Constructor
+    Event(int par_event_time, int par_customer_id) {
         event_time = par_event_time;
         customer_id = par_customer_id;
     }
 };
 
-void initialize_system(
-    std::ifstream& in_file,
-    std::deque<Event>& arrival_events,
-    std::vector<Customer>& customers)
-{
+// Initialize System Method
+void initialize_system(std::ifstream& in_file, std::deque<Event>& arrival_events, std::vector<Customer>& customers) {
+    // Method Variables
     std::string name;
     int priority, arrival_time, slots_requested;
-
     // read input file line by line
     // https://stackoverflow.com/questions/7868936/read-file-line-by-line-using-ifstream-in-c
     int customer_id = 0;
-    while (in_file >> name >> priority >> arrival_time >> slots_requested)
-    {
+    while (in_file >> name >> priority >> arrival_time >> slots_requested) {
         Customer customer_from_file(name, priority, arrival_time, slots_requested);
         customers.push_back(customer_from_file);
-
         // new customer arrival event
         Event arrival_event(arrival_time, customer_id);
         arrival_events.push_back(arrival_event);
-
         customer_id++;
     }
 }
 
-void print_state(
-    std::ofstream& out_file,
-    int current_time,
-    int current_id,
-    const std::deque<Event>& arrival_events,
-    const std::deque<int>& customer_queue)
-{
+// Print State (Prints the state of the program) (Outputs to the console)
+void print_state(std::ofstream& out_file, int current_time, int current_id, const std::deque<Event>& arrival_events, const std::deque<int>& customer_queue) {
     out_file << current_time << " " << current_id << '\n';
     if (PRINT_LOG == 0)
     {
@@ -93,8 +84,7 @@ void print_state(
 
 // process command line arguments
 // https://www.geeksforgeeks.org/command-line-arguments-in-c-cpp/
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     if (argc != 3)
     {
         std::cerr << "Provide input and output file names." << std::endl;
@@ -107,25 +97,21 @@ int main(int argc, char* argv[])
         std::cerr << "Cannot open one of the files." << std::endl;
         return -1;
     }
-
+    // Method Variables
     // deque: https://www.geeksforgeeks.org/deque-cpp-stl/
     // vector: https://www.geeksforgeeks.org/vector-in-cpp-stl/
     std::deque<Event> arrival_events; // new customer arrivals
     std::vector<Customer> customers; // information about each customer
-
     // read information from file, initialize events queue
     initialize_system(in_file, arrival_events, customers);
-
     std::deque<int> arrival_queue;
     std::deque<int> high_priority_queue;
     std::deque<int> low_priority_queue;
-
     std::deque<int>* current_queue = &arrival_queue;
-
     int current_id = -1;
     int time_out = -1;
     bool all_done = false;
-
+    // Loops through until all_done is true
     for (int current_time = 0; !all_done; current_time++) {
         while (!arrival_events.empty() && (current_time == arrival_events[0].event_time)) {
             arrival_queue.push_back(arrival_events[0].customer_id);
@@ -148,6 +134,7 @@ int main(int argc, char* argv[])
             }
         }
         if (current_id == -1) {
+            // If the current_queue is empty
             if (current_queue->empty()) {
                 if (current_queue == &arrival_queue) {
                     current_queue = &high_priority_queue;
@@ -174,6 +161,7 @@ int main(int argc, char* argv[])
                         });
                 }
             }
+            // If the current_queue is NOT empty
             if (!current_queue->empty()) {
                 current_id = current_queue->front();
                 current_queue->pop_front();
@@ -191,10 +179,9 @@ int main(int argc, char* argv[])
                 customers[current_id].playing_since = current_time;
             }
         }
+        // Runs the print_state method
         print_state(out_file, current_time, current_id, arrival_events, *current_queue);
-
         all_done = (arrival_events.empty() && arrival_queue.empty() && high_priority_queue.empty() && low_priority_queue.empty() && (current_id == -1));
     }
-
     return 0;
 }
